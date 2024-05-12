4726a6857c474934ef2534d7cc3cb03393d17748
Marcocholla01 <marcochollapaul01@gmail.com>
2024-05-13 02:56:48 +0300

    Remove HelloWorld component and add Reaction Timer components
    The HelloWorld component has been removed and replaced with the Reaction Timer components, including the App component and Block component. The App component now displays a title, start button, and added on click events and v-binding on the block component

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" v-bind:delay="delay" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Block from "./components/Block.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Block },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+    };
+  },
+  methods: {
+    start() {
+      (this.isPlaying = true), (this.delay = 2000 + Math.random() * 5000);
+      // console.log(this.delay);
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +32,7 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
 }
 </style>
---
+++ src/components/Block.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="block">click me</div>
+</template>
+<script>
+export default {
+  props: ["delay"],
+};
+</script>
+<style scoped>
+#block {
+  width: 400px;
+  border-radius: 20px;
+  background: #0faf87;
+  color: white;
+  text-align: center;
+  padding: 100px 0px;
+  margin: 40px auto;
+}
+</style>
